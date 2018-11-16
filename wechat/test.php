8b0dcac4fc5acae1cedc2530454f2058bd52f077
JackChaoZhang <43022462+JackChaoZhang@users.noreply.github.com>
2018-11-16 17:58:58 +0800

    Update test.php

--- wechat/test.php
+++ wechat/test.php
@@ -1,16 +1,5 @@
 <?php
-$con = mysql_connect("localhost","lxiwiuo","mySQL_zhch2004");
-if (!$con)
-  {
-  //die('Could not connect: ' . mysql_error());
-  echo mysql_error();
-  }
 
-mysql_select_db("lxiwiuo", $con);
+phpinfo();
 
-$result = mysql_query("SELECT * FROM article");
-
-echo json_encode(mysql_fetch_array($result));
-
-mysql_close($con);
 ?>
