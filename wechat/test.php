77f8ae8c320897f4c67efedae38f92be0449d401
JackChaoZhang <43022462+JackChaoZhang@users.noreply.github.com>
2018-11-16 17:18:14 +0800

    Create test.php

---
+++ wechat/test.php
@@ -0,0 +1,15 @@
+<?php
+$con = mysql_connect("localhost","lxiwiuo","mySQL_zhch2004");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("lxiwiuo", $con);
+
+$result = mysql_query("SELECT * FROM article");
+
+echo json_encode(mysql_fetch_array($result));
+
+mysql_close($con);
+?>
